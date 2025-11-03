6fa66f2c321a506154202dd5e9fac20de5f3298c
Nahuel Zárate <162528571+ZNahuel7@users.noreply.github.com>
2025-11-02 22:05:43 -0300

    Update mensaje.py

--- mensaje.py
+++ mensaje.py
@@ -1,10 +1,11 @@
 # Esta clase representa un mensaje enviado entre usuarios.
 class Mensaje:
-    def __init__(self, emisor, receptor, asunto, cuerpo):
+    def __init__(self, emisor, receptor, asunto, cuerpo, prioridad=0):
         self._emisor = emisor
         self._receptor = receptor
         self._asunto = asunto
         self._cuerpo = cuerpo
+        self._prioridad = prioridad  # Si es 0,entonces la prioridad es normal. Si es 1, la prioridad es urgente
 
     # Getters para acceder a los datos privados del mensaje
     def get_emisor(self):
@@ -25,3 +26,10 @@ class Mensaje:
 
     def set_cuerpo(self, nuevo_cuerpo):
         self._cuerpo = nuevo_cuerpo
+        
+    def get_prioridad(self):
+        return self._prioridad
+        
+    def set_prioridad(self, nueva_prioridad):
+        if nueva_prioridad in [0, 1]:  # condicional para permitir solo prioridad normal (0) o urgente (1)
+            self._prioridad = nueva_prioridad
