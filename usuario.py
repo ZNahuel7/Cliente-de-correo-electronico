b49385152d885cd504de8b465ff8a0907963f2a2
Nahuel Zárate <162528571+ZNahuel7@users.noreply.github.com>
2025-11-02 22:07:20 -0300

    Update usuario.py

--- usuario.py
+++ usuario.py
@@ -1,14 +1,17 @@
 from carpeta import Carpeta
 
-# Clase que representa a un usuario del sistema de correo electronico.
+# Clase que representa a un usuario del sistema de correo.
 class Usuario:
+    # Análisis de eficiencia:
+    # Buscar mensajes en todas las carpetas es O(m) donde m es la cantidad total de mensajes del usuario.
+    # Mover mensajes depende de la profundidad del árbol de carpetas.
     def __init__(self, nombre, correo):
         self._nombre = nombre
         self._correo = correo
         self._buzon_entrada = Carpeta("Bandeja de entrada")
-        self._carpetas = [self._buzon_entrada]  # Lista de carpetas del usuario
+        self._carpetas = [self._buzon_entrada]  # Lista de carpetas principales del usuario
 
-    # Getters que permiten acceder a los datos privados del usuario.
+    # Getters que permiten acceder a los datos privados del usuario de forma controlada
     def get_nombre(self):
         # Devuelve el nombre del usuario
         return self._nombre
@@ -46,5 +49,4 @@ class Usuario:
     def recibir_mensaje(self, mensaje):
         #Agrega un mensaje a la bandeja de entrada del usuario.
         self._buzon_entrada.agregar_mensaje(mensaje)
-
-
+        
